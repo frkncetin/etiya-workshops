decc3fff7342e58e1684e2a377150a414cf3b78a
Furkan Cetin <frkncetin77mail.com>
2023-04-06 14:44:41 +0300

    düzeltme

--- workshop2.py
+++ workshop2.py
@@ -1,3 +1,12 @@
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final + 0.6)
+
+#eğer final 40'dan küçükse kullanıcı kaldı
+#eğer ortalama 50'den küçükse kullanıcı kaldı
+#eğer vize finalin 2 katı ise kullanıcı kaldı
+#bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyorum
+
 if ortalama < 50 or final < 40 or vize > (final*2):
     print("Kullanıcı Kaldı")
 else:
