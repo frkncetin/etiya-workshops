c4f7860edd4baebda3d70c940d02cfb3bb54f4be
Furkan Cetin <frkncetin77mail.com>
2023-04-11 11:56:16 +0300

    paketler

--- class-modul-package/modules.py
+++ class-modul-package/modules.py
@@ -10,9 +10,14 @@ from classlar import Human
 print(toplamaIslemi(10,20))
 
 import random
+from selenium import webdriver
+
 #package
 
 print(random.randint(0,100))
 
 human1 = Human("Mirza")
 human1.talk("Merhaba")
+
+chromeDriver = webdriver.Chrome()
+#packages
